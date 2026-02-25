7afcb473dc5dbcb0e050fe2b0bc06fb880fade92
UtasPrasojo <utas.prasojo@varx.id>
2026-02-25 16:20:06 +0700

    feat:setup react and tailwind

---
+++ src/App.test.jsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import App from './App';
+
+describe('Aplikasi Utama', () => {
+  it('harus menampilkan teks Vite + React', () => {
+    render(<App />);
+    expect(screen.getByText(/Vite \+ React/i)).toBeInTheDocument();
+  });
+});
